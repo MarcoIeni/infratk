9077a24eadc80401acce9d1734aed0191291db8b
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-08-01 09:26:06 +0200

    use lock

--- src/terragrunt.rs
+++ src/terragrunt.rs
@@ -9,21 +9,15 @@ pub fn are_changes_applied(state: &Utf8Path) -> bool {
     // 0 - Succeeded, diff is empty (no changes)
     // 1 - Errored
     // 2 - Succeeded, there is a diff
-    let output = Cmd::new(
-        "terragrunt",
-        ["plan", "-detailed-exitcode", "-lock=false", "-input=false"],
-    )
-    .with_current_dir(state)
-    .run();
+    let output = Cmd::new("terragrunt", ["plan", "-detailed-exitcode", "-input=false"])
+        .with_current_dir(state)
+        .run();
     output.status().code().unwrap() == 0
 }
 
 pub fn terragrunt_init_upgrade(state: &Utf8Path) {
-    let output = Cmd::new(
-        "terragrunt",
-        ["init", "--upgrade", "-lock=false", "-input=false"],
-    )
-    .with_current_dir(state)
-    .run();
+    let output = Cmd::new("terragrunt", ["init", "--upgrade", "-input=false"])
+        .with_current_dir(state)
+        .run();
     assert!(output.status().success());
 }
